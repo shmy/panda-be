f4391a7d289faa742d8faf9b07f1f7c5c70cffc5
“wjincheng” <2411465039@qq.com>
2020-01-03 10:07:14 +0800

    hello world

---
+++ src/main/java/com/tw/panda/panda/PandaApplication.java
@@ -0,0 +1,13 @@
+package com.tw.panda.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaApplication.class, args);
+	}
+
+}
